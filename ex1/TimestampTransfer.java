feb41d4a90cd661b6fd26e032103f8ff2b08a1f1
WilliamKZhy <894494132@qq.com>
2016-11-03 17:00:35 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -13,23 +13,24 @@ import java.util.Scanner;
  * 2. 对代码进行注释，说明每行代码的作用，把代码提交到个人分支下
  * 3. 创建pull request，与主仓库的master分支对比
  */
-public class TimestampTransfer {
-	@SuppressWarnings("resource")
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			Date lineDate = null;
-			long lineTimestamp;
-			try {
-				lineDate = inputFormat.parse(line);
-				lineTimestamp = lineDate.getTime();
-				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+	+public class TimestampTransfer {   //创建类
+ 	@SuppressWarnings("resource")      //错误自动注释
+ 	public static void main(String[] args){    //主函数
+ 		Scanner scanner = new Scanner(System.in);   //创建一个Scanner输入
+ 		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");    //创建一个输入，格式是“年-月-日 时：分：秒”
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");   //创建一个输出，格式是“年-月-日 时：分：秒”
+ 		while (scanner.hasNext()){  //循环获取输入，当没有下一个输入时结束
+ 			String line = scanner.nextLine();   //创建一个字符串变量，用于获取读入的数据
+ 			Date lineDate = null;      //创建一个日期变量，初始值为空
+ 			long lineTimestamp;     //创建一个长整型变量，用于存储时间的计算值
+ 			try {                    //捕获异常
+ 				lineDate = inputFormat.parse(line);     //把读取的字符串数据中的日期放入日期变量中
+ 				lineTimestamp = lineDate.getTime();    //把获取到的日期计算后赋值给长整型变量
+ 				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);  //输出获取的日期和计算后的时间值
+ 			} catch (ParseException e) {   //如果异常则输出下面错误
+  				// TODO Auto-generated catch block
+ -				e.printStackTrace();
 			}
 		}
 	}
